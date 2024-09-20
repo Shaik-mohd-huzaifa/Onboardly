eb2cc6f57c119f08171228bcc8979db6818a5651
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2024-09-21 01:46:07 +0530

    Fix Env variables

--- server/utils/tokenGen.py
+++ server/utils/tokenGen.py
@@ -1,9 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Authenticator:
     def __init__(self, api_key):
         self.api_key = api_key
-        
+
     def get_access_token(self):
         token_url = "https://iam.cloud.ibm.com/identity/token"
         header = {
@@ -11,12 +15,11 @@ class Authenticator:
         }
         body = {
             "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
-            "apikey": "0aUDde75uvJZuPgw7hWYjVsfwW6_YL0yOOd_mStnqmYE"
+            "apikey": os.getenv("IBM_WATSONX_API_KEY"),
         }
         response = requests.post(url=token_url, headers=header,data=body)
-        
+
         if response.status_code == 200:
             return response.json()["access_token"]
         else: 
-            raise Exception(response.text)  
-    
+            raise Exception(response.text)
